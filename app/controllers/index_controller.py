a62d5b8ab57df6ba2f089118a26c26af3eec249e
Preet S <w3irdg@gmail.comm>
2022-02-03 13:46:10 -0500

    Edited name variable

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Keith Williams"
+        name = "Pretpal Singh"
         return render_template('index.html', name=name)
